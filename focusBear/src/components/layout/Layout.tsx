e6002149cc2510458b5bbe7dfac0ff6876e6fd7c
JoonChun <jeonjun47@gmail.com>
2026-03-28 21:19:34 +0900

    feat: add Layout, Header, Sidebar components

---
+++ focusBear/src/components/layout/Layout.tsx
@@ -0,0 +1,26 @@
+// src/components/layout/Layout.tsx
+import { ReactNode, useEffect } from 'react'
+import { Sidebar } from './Sidebar'
+import { Header } from './Header'
+import { useApp } from '../../context/AppContext'
+import { getTodayTotal } from '../../db/sessions'
+import { seedDefaultCategories } from '../../db/schema'
+
+export function Layout({ children }: { children: ReactNode }) {
+  const { dispatch } = useApp()
+
+  useEffect(() => {
+    seedDefaultCategories()
+    getTodayTotal().then(total => dispatch({ type: 'SET_TODAY_TOTAL', total }))
+  }, [dispatch])
+
+  return (
+    <div className="min-h-screen bg-[var(--bg-color)]">
+      <Header />
+      <Sidebar />
+      <main className="ml-16 pt-14 min-h-screen">
+        {children}
+      </main>
+    </div>
+  )
+}
