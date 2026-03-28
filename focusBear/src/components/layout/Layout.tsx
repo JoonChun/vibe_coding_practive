494232561c8b7824097d4d80c0a624a439e7ccad
JoonChun <jeonjun47@gmail.com>
2026-03-29 01:37:22 +0900

    feat: load categories into context on app init

--- focusBear/src/components/layout/Layout.tsx
+++ focusBear/src/components/layout/Layout.tsx
@@ -6,12 +6,16 @@ import { Header } from './Header'
 import { useApp } from '../../context/AppContext'
 import { getTodayTotal } from '../../db/sessions'
 import { seedDefaultCategories } from '../../db/schema'
+import { getCategories } from '../../db/categories'
 
 export function Layout({ children }: { children: ReactNode }) {
   const { dispatch } = useApp()
 
   useEffect(() => {
     seedDefaultCategories()
+      .then(() => getCategories())
+      .then(cats => dispatch({ type: 'SET_CATEGORIES', categories: cats }))
+      .catch(err => console.error('Failed to initialize categories:', err))
     getTodayTotal().then(total => dispatch({ type: 'SET_TODAY_TOTAL', total }))
   }, [dispatch])
 
