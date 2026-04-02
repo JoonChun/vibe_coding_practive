7ee483974431b4de3b4f991f7db0a660eea5941f
JoonChun <jeonjun47@gmail.com>
2026-04-03 01:41:47 +0900

    chore: bump version to 3.0.0

--- focusBear/src/components/settings/SettingsView.tsx
+++ focusBear/src/components/settings/SettingsView.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useApp } from '../../context/AppContext'
 import { ExportImport } from './ExportImport'
 import { CategoryManager } from './CategoryManager'
